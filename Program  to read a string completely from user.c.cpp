94cba0530a8a5a38a82d7d01adbda50b175769c2
Chinta.Madhu Latha <chintamadhulatha11@gmail.com>
2025-11-26 11:53:38 +0530

    Delete Program  to read a string completely from user.c.cpp

--- Program  to read a string completely from user.c.cpp
+++
@@ -1,9 +0,0 @@
-//Program to read a string completely from user
-#include<stdio.h>
-int main()
-{
-	char name[20];
-	printf("enter your name");
-	scanf("%[^\n]s", name);
-	printf("my name is %s", name);
-}
